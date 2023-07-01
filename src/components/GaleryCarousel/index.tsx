aa067fd00387be09a65e7222733726289ef10660
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-07-01 19:24:00 -0300

    m

--- src/components/GaleryCarousel/index.tsx
+++ src/components/GaleryCarousel/index.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import { Container } from "./styles";
 
 export default function GaleryCarousel() {
   return (
     <Container>
-      
+      <Image src={''} alt="" width={370} height={370} />
+      <div className="heart-icon">
+
+      </div>
+      <div className="mensager-icon">
+
+      </div>
+      <p></p>
+      <span></span>
     </Container>
   )
 }
