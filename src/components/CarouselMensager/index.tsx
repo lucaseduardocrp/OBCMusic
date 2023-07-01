e765584c004aee08afe4552c4876782641cb153b
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-07-01 13:21:11 -0300

    m

--- src/components/CarouselMensager/index.tsx
+++ src/components/CarouselMensager/index.tsx
@@ -7,25 +7,25 @@ import { PeopleReports } from "@/modules/PeopleReports";
 export default function CarouselMensager() {
   return (
     <Container>
-      <Mensager>
-        <div className="quoteOpen">
-          <QuotesOpenIcon />
-        </div>
-          {PeopleReports.map((item, id) => 
+      {PeopleReports.map((item, id) => 
+        <>
+          <Mensager key={id}>
+            <div className="quoteOpen">
+              <QuotesOpenIcon />
+            </div>
             <Text key={id}>
               {item.description}
             </Text>
-          )}
-          <div className="quoteClose">
-            <QuotesCloseIcon />
+            <div className="quoteClose">
+              <QuotesCloseIcon />
+            </div>
+          </Mensager>
+          <div className="people-reports">
+            <Image src={item.people} alt={item.name} width={120} height={120} />
+            <h4>{item.name}</h4>
+            <span>{item.local}</span>
           </div>
-      </Mensager>
-      {PeopleReports.map((item, id) => 
-        <div className="people-reports" key={id}>
-          <Image src={item.people} alt={item.name} width={120} height={120} />
-          <h4>{item.name}</h4>
-          <span>{item.local}</span>
-        </div>
+        </>
       )}
     </Container>
   )
