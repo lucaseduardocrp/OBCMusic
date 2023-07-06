a777ce21de1e42569c990cd4fd50c4203a7e15ac
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-07-06 11:38:42 -0300

    a

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
@@ -31,9 +32,38 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet" />
+          {/* Primary Meta Tags */}
+          <meta name="title" content="Onebitmusic — A musical immersion" />
+          <meta name="description" content="A Sua Melhor Experiência
+          Musical Digital.
+          Prepare-se para uma experiência incrível e envolvente, onde a arte se une à performance para criar momentos inesquecíveis. Nossos shows artísticos são projetados para encantar, inspirar e transportar você para um mundo de criatividade e expressão.
+          " />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://metatags.io" />
+          <meta property="og:title" content="Onebitmusic — A musical immersion" />
+          <meta property="og:description" content="A Sua Melhor Experiência
+          Musical Digital.
+          Prepare-se para uma experiência incrível e envolvente, onde a arte se une à performance para criar momentos inesquecíveis. Nossos shows artísticos são projetados para encantar, inspirar e transportar você para um mundo de criatividade e expressão.
+          " />
+          <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://metatags.io" />
+          <meta property="twitter:title" content="Onebitmusic — A musical immersion" />
+          <meta property="twitter:description" content="A Sua Melhor Experiência
+          Musical Digital.
+          Prepare-se para uma experiência incrível e envolvente, onde a arte se une à performance para criar momentos inesquecíveis. Nossos shows artísticos são projetados para encantar, inspirar e transportar você para um mundo de criatividade e expressão.
+          " />
+          <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet" />
+
+          <title> Onebitmusic — A musical immersion </title>
         </Head>
         <body>
           <Main />
